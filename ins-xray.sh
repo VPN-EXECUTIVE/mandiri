cc6f37e64b5144b5a1ab29ee1da99e56a4b1e2a3
VPN-EXECUTIVE <123308175+VPN-EXECUTIVE@users.noreply.github.com>
2024-02-19 01:17:31 +0700

    Add files via upload

--- ins-xray.sh
+++ ins-xray.sh
@@ -496,31 +496,31 @@ systemctl restart runn
 
 cd /usr/bin/
 # vmess
-wget -O add-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/add-ws.sh" && chmod +x add-ws
-wget -O trialvmess "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/trialvmess.sh" && chmod +x trialvmess
-wget -O renew-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/renew-ws.sh" && chmod +x renew-ws
-wget -O del-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/del-ws.sh" && chmod +x del-ws
-wget -O cek-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/cek-ws.sh" && chmod +x cek-ws
+wget -O add-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/add-ws.sh" && chmod +x add-ws
+wget -O trialvmess "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/trialvmess.sh" && chmod +x trialvmess
+wget -O renew-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/renew-ws.sh" && chmod +x renew-ws
+wget -O del-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/del-ws.sh" && chmod +x del-ws
+wget -O cek-ws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/cek-ws.sh" && chmod +x cek-ws
 
 # vless
-wget -O add-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/add-vless.sh" && chmod +x add-vless
-wget -O trialvless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/trialvless.sh" && chmod +x trialvless
-wget -O renew-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/renew-vless.sh" && chmod +x renew-vless
-wget -O del-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/del-vless.sh" && chmod +x del-vless
-wget -O cek-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/cek-vless.sh" && chmod +x cek-vless
+wget -O add-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/add-vless.sh" && chmod +x add-vless
+wget -O trialvless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/trialvless.sh" && chmod +x trialvless
+wget -O renew-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/renew-vless.sh" && chmod +x renew-vless
+wget -O del-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/del-vless.sh" && chmod +x del-vless
+wget -O cek-vless "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/cek-vless.sh" && chmod +x cek-vless
 
 # trojan
-wget -O add-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/add-tr.sh" && chmod +x add-tr
-wget -O trialtrojan "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/trialtrojan.sh" && chmod +x trialtrojan
-wget -O del-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/del-tr.sh" && chmod +x del-tr
-wget -O renew-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/renew-tr.sh" && chmod +x renew-tr
-wget -O cek-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/cek-tr.sh" && chmod +x cek-tr
+wget -O add-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/add-tr.sh" && chmod +x add-tr
+wget -O trialtrojan "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/trialtrojan.sh" && chmod +x trialtrojan
+wget -O del-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/del-tr.sh" && chmod +x del-tr
+wget -O renew-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/renew-tr.sh" && chmod +x renew-tr
+wget -O cek-tr "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/cek-tr.sh" && chmod +x cek-tr
 
 # shadowsocks
-wget -O add-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/add-ssws.sh" && chmod +x add-ssws
-wget -O trialssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/trialssws.sh" && chmod +x trialssws
-wget -O del-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/del-ssws.sh" && chmod +x del-ssws
-wget -O renew-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/renew-ssws.sh" && chmod +x renew-ssws
+wget -O add-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/add-ssws.sh" && chmod +x add-ssws
+wget -O trialssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/trialssws.sh" && chmod +x trialssws
+wget -O del-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/del-ssws.sh" && chmod +x del-ssws
+wget -O renew-ssws "https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/renew-ssws.sh" && chmod +x renew-ssws
 
 
 sleep 0.5
