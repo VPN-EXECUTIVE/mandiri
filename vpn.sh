4531723ca45913855bc8188fd4555756e1423253
VPN-EXECUTIVE <123308175+VPN-EXECUTIVE@users.noreply.github.com>
2024-02-18 14:48:05 +0700

    Add files via upload

--- vpn.sh
+++ vpn.sh
@@ -18,7 +18,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.github.com/VPN-EXECUTIVE/mandiri/udp-custom/openvpn/vpn.zip
+wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/udp-custom/openvpn/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
