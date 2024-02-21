168da3eb1256d7c55d4505ad5bb9f63b11d03fdd
VPN-EXECUTIVE <123308175+VPN-EXECUTIVE@users.noreply.github.com>
2024-02-21 17:31:22 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -124,7 +124,7 @@ echo -e "$BGreen 2. Choose Your Own Domain / Gunakan Domain Sendiri $NC"
 echo -e "$BYellow----------------------------------------------------------$NC"
 read -rp " input 1 or 2 / pilih 1 atau 2 : " dns
 if test $dns -eq 1; then
-wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/cf && chmod +x cf && ./cf
+wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/ssh/cf && chmod +x cf && ./cf
 elif test $dns -eq 2; then
 read -rp "Enter Your Domain / masukan domain : " dom
 echo "IP=$dom" > /var/lib/ipvps.conf
@@ -141,14 +141,21 @@ echo -e "${BGreen}Done!${NC}"
 sleep 2
 clear
     
+#install ssh ovpn
+echo -e "\e[33m-----------------------------------\033[0m"
+echo -e "$BGreen      Install SSH Websocket           $NC"
+echo -e "\e[33m-----------------------------------\033[0m"
+sleep 0.5
+clear
+wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #Instal Xray
 echo -e "\e[33m-----------------------------------\033[0m"
 echo -e "$BGreen          Install XRAY              $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -171,6 +178,9 @@ fi
 if [ -f "/etc/afak.conf" ]; then
 rm /etc/afak.conf > /dev/null 2>&1
 fi
+if [ ! -f "/etc/log-create-ssh.log" ]; then
+echo "Log SSH Account " > /etc/log-create-ssh.log
+fi
 if [ ! -f "/etc/log-create-vmess.log" ]; then
 echo "Log Vmess Account " > /etc/log-create-vmess.log
 fi
@@ -184,7 +194,7 @@ if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
 echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/VPN-EXECUTIVE/mandiri/master/menu/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
@@ -232,7 +242,7 @@ echo "   - Trojan gRPC              : 443" | tee -a log-install.txt
 echo "   - Shadowsocks gRPC         : 443" | tee -a log-install.txt
 echo ""
 echo "=============================Contact==============================" | tee -a log-install.txt
-echo "--------------------------VPN-EXECUTIVE-----------------------------" | tee -a log-install.txt
+echo "---------------------------t.me/givpn-----------------------------" | tee -a log-install.txt
 echo "==================================================================" | tee -a log-install.txt
 echo -e ""
 echo ""
